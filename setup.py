c543ffa2a45fb8bf9ab085dd5e3fd1e18ffce5d7
S4qib <73705449+S4qib@users.noreply.github.com>
2021-05-06 21:07:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,27 @@
-import setuptools
+  
+from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8", errors="ignore") as fh:
-    long_description = fh.read()
+    readme = fh.read()
 
-setuptools.setup(
-    name="MechaKaren.py",
-    version="1.0",
-    author="Daftscientist", "S4qib",
-    description="Free to use API Wrapper for MechaKaren API.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Rapi-Dev/mechakaren.py/",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>= 3.6',
-    include_package_data=True,
-    install_requires=["requests", "Colorama"]
+setup(name='MechaKaren.py',
+      author=['Daftscientist', 'S4qib'],
+      url='https://github.com/Rapi-Dev/MechaKaren.py/',
+      version='1.0',
+      packages=['MechaKaren'],
+      license='MIT',
+      description='A Python wrapper for the MechaKaren API',
+      long_description=readme,
+      long_description_content_type="text/x-rst",
+      include_package_data=True,
+      install_requires=['requests', 'Colorama'],
+      python_requires='>=3.8.0',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+      ]
 )
