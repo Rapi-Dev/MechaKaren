bc6dd8547aadbd639c4a7215cb1636136c55f32a
Daftscientist <67394753+Daftscientist@users.noreply.github.com>
2021-05-08 13:59:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,23 @@
-  
-from setuptools import setup
+import setuptools
 
 with open("README.md", "r", encoding="utf-8", errors="ignore") as fh:
-    readme = fh.read()
+    long_description = fh.read()
 
-setup(name='MechaKaren.py',
-      author=['Daftscientist', 'S4qib'],
-      url='https://github.com/Rapi-Dev/MechaKaren.py/',
-      version='1.0',
-      packages=['MechaKaren'],
-      license='MIT',
-      description='A Python wrapper for the MechaKaren API',
-      long_description=readme,
-      long_description_content_type="text/x-rst",
-      include_package_data=True,
-      install_requires=['requests', 'Colorama'],
-      python_requires='>=3.8.0',
-      classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-      ]
+setuptools.setup(
+    name="MechaKaren",
+    version="1.0",
+    author=["RapiDevelopment"],
+    description="Free to use API Wrapper for MechaKaren API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Rapi-Dev/MechaKaren.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>= 3.6',
+    include_package_data=True,
+    install_requires=["requests", "Colorama"]
 )
